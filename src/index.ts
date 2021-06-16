394c2695ddbaeaa8b00a416d972a2ca19c04867e
Soheil ASR <asrsoheil@gmail.com>
2021-06-16 17:18:28 +0430

    feat: Add project index file

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import countries from "./data/countries";
+import { getCountry, getCountryByDialCode } from "./getCountry";
+import { getFullSvgUrl } from "./svgUrl";
+
+export { getCountry, getFullSvgUrl, getCountryByDialCode };
+export default countries;
