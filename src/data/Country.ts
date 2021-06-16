e1ff9e8df31e56d5786e7c3237f6001d78381acd
Soheil ASR <asrsoheil@gmail.com>
2021-06-16 17:14:44 +0430

    feat: Add all countries info and type

---
+++ src/data/Country.ts
@@ -0,0 +1,13 @@
+export interface Country {
+  alpha2: string;
+  alpha3: string;
+  name: {
+    en: string;
+    fa: string;
+  };
+  dialCode: string;
+  svgUrl: string;
+  emoji: string;
+  unicode: string;
+  numericCode: string;
+}
